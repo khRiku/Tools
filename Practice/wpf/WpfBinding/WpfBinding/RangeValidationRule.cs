0e445ab8e65a193ecd9d6628c9eeaeb25e118dfb
khRiku <1572883601@qq.com>
2019-04-30 08:51:09 +0800

    Update：binding 例子

---
+++ Practice/wpf/WpfBinding/WpfBinding/RangeValidationRule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfBinding
+{
+	class RangeValidationRule : ValidationRule
+	{
+		public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+		{
+			double d = 0;
+			if (double.TryParse(value.ToString(), out d))
+			{
+				if (d >= 0 && d <= 100)
+				{
+					return new ValidationResult(true, null);
+				}
+			}
+			return new ValidationResult(false, "ErrorContent");
+		}
+	}
+}
